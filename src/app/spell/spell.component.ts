9f4a2caf9c9dc484d02737dc0214798c70369dbf
Ashwini <ashjd29@gmail.com>
2016-06-30 19:56:32 -0700

    (feat) basic spelling page

--- src/app/spell/spell.component.ts
+++ src/app/spell/spell.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { WordsService } from './wordsService.service';
 
 @Component({
   selector: 'spell',
   template: require('./spell.component.html'),
-  styles: [require('./spell.component.css')]
+  styles: [require('./spell.component.css')],
+  providers: [WordsService]
 })
 export class Spell implements OnInit {
 
-  constructor() {}
+  private spellingWord:string = '';
+
+  constructor( private wordsService: WordsService) {}
 
   ngOnInit() {
+    this.spellingWord = this.wordsService.returnRandomWord();
   }
 
 }
